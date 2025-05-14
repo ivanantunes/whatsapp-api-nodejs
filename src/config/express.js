67da44468202e1b0c2d96318eea0ea4d043f47a4
Jeanluca <jeanlucafp@gmail.com>
2025-05-14 14:22:06 -0300

    Add CORS middleware to Express configuration

--- src/config/express.js
+++ src/config/express.js
@@ -6,9 +6,11 @@ const app = express()
 const error = require('../api/middlewares/error')
 const tokenCheck = require('../api/middlewares/tokenCheck')
 const { protectRoutes } = require('./config')
+const cors = require('cors')
 
 require('./swagger')(app)
 
+app.use(cors())
 app.use(express.json())
 app.use(express.json({ limit: '50mb' }))
 app.use(express.urlencoded({ extended: true }))
